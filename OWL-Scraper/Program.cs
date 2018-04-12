c42039001947e6a85ebd6c804da975a65e853358
wesbosse <wes.bosse@gmail.com>
2018-04-12 07:52:07 -0700

    first commit

---
+++ OWL-Scraper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OWLScraper
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
